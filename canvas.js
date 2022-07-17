bb4a3236a8f4495dd9e783be47d7121971de4262
andruss93 <you@example.com>
2022-07-17 16:04:40 -0500

    iniciando estilo, solo se le hizo mas grande el ahorcado y se cambio el color de las letras

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 let canvas= document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
-ctx.canvas.width = 0
-ctx.canvas.height = 0
+//ctx.canvas.width = 0
+//ctx.canvas.height = 0
 
 
 const bodyParts = [
@@ -18,10 +18,10 @@ const addBodyPart = bodyPart => {
     ctx.fillRect(...bodyPart)
 }
 
-const dibujarAhorcado = () =>{
-    ctx.canvas.width = 120
-    ctx.canvas.height = 160
-    ctx.scale (20,20)
+function dibujarAhorcado (){
+    ctx.canvas.width = 1200
+    ctx.canvas.height = 860
+    ctx.scale (50,50)
     ctx.clearRect(0,0, canvas.width, canvas.height)
     ctx.fillStyle = 'black'
     ctx.fillRect (0,7,4,1)
@@ -29,3 +29,23 @@ const dibujarAhorcado = () =>{
     ctx.fillRect (2,0,3,1)
     ctx.fillRect (4,1,1,1)
 }
+
+/*function dibujarLineas(){
+    canvas.lineWidth = 6
+    canvas.lineCap = "round"
+    canvas.lineJoin ="round"
+    canvas.fillStyle = 'green'
+    canvas.beginPath()
+
+    var largo = 5
+    var ancho = 600/largo
+    for(var i =0; i< seleccionarPalabra.length;i++){
+        ctx.canvas.moveTo(500+(ancho*i),640)
+        ctx.canvas.lineTo(550+(ancho*i),640)
+    }
+       ctx.canvas.stroke()
+       ctx.canvas.closePath()
+       console.log(seleccionarPalabra)
+}
+dibujarLineas(largo)
+*/
